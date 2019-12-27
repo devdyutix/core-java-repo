87a858fc43a4d7697b2b9e17154a898ee5927ac3
Devdyuti <singhking5414@hotmail.com>
2019-12-27 15:29:22 +0530

    added forkJoin example

--- routine-core-practice/src/main/java/org/dev/thread/concurrency/ForkJJoinTest.java
+++ routine-core-practice/src/main/java/org/dev/thread/concurrency/ForkJJoinTest.java
@@ -1,5 +1,55 @@
 package org.dev.thread.concurrency;
 
-public class ForkJJoinTest {
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
 
+public class ForkJJoinTest{
+	
+	static class Sum extends RecursiveTask<Long>{ 
+		
+		// RecursiveAction represents a task which doesn't return any value
+		// RecursiveTask represents a task which returns a long value.
+		
+		int low;
+		int high;
+		int arr[];
+		
+		public Sum(int[] arr,int low,int high) {
+			this.low = low;
+			this.high = high;
+			this.arr = arr;
+		}
+		@Override
+		protected Long compute() {
+			if(high-low<=10) {
+				long sum=0;
+				for(int i=low;i<high; ++i) 
+					sum+=arr[i];
+				return sum;
+			}else {
+				int mid=low+(high-low)/2;
+				Sum left=new Sum(arr, low, mid);
+				Sum right=new Sum(arr, mid, high);
+				left.fork(); // Split the task into the subtasks.
+				long righResult=right.compute();
+				long leftResult=left.join(); // Joins all results of subtasks once the subtask finished executing, otherwise keep waiting. 
+				return leftResult+righResult;
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		int nThreads=Runtime.getRuntime().availableProcessors();
+		System.out.println("Available processors:"+nThreads);
+		int arr[]=new int[1000];
+		int sum=0;
+		for(int i=0;i<arr.length;i++) {
+			arr[i]=i;
+			sum+=i;
+		}
+		ForkJoinPool forkJoinPool=new ForkJoinPool(nThreads);// Special Thread pool designated to work with fork-and-join task splitting.  
+		long result=forkJoinPool.invoke(new Sum(arr, 0, arr.length));
+		System.out.println(result);
+	}
 }
