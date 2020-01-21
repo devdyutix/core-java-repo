644617190f8f9b942fdb09e9b91f149da0965c93
Devdyuti <singhking5414@hotmail.com>
2020-01-21 09:15:13 +0530

    update revision test

--- routine-core-practice/src/main/java/org/dev/thread/RevisionTest.java
+++ routine-core-practice/src/main/java/org/dev/thread/RevisionTest.java
@@ -2,32 +2,63 @@ package org.dev.thread;
 /*
  * @ Example DeadLock
  */
-class Testing{
-	private static Object resource=new Object();
-	public void go(){
-		Runnable r=new Runnable() {
+class Displayy{
+	public synchronized void wishm1(String name) {
+		for (int i = 0; i < 10; i++) {
+			System.out.print("Synchronized instance metod, GOOD MORING: ");
+			try {
+				Thread.sleep(1000);	
+			}catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println(name);
 			
-			@Override
-			public void run() {
-				System.out.println("foo");
-				
+		}
+	}
+	public static synchronized void wishm2(String name) {
+		for (int i = 0; i < 10; i++) {
+			System.out.print("static synchronized method GOOD MORING: ");
+			try {
+				Thread.sleep(1000);	
+			}catch (InterruptedException e) {
+				e.printStackTrace();
 			}
-		};
-/*		new Thread(r){
-			public void run() {
-				System.out.println("bar");
-			}
-		}.start();*/
-		Thread t=new Thread(r);
-		t.start();
-		
+			System.out.println(name);
+			
+		}
+	}
+}
+class Testing extends Thread{
+	
+	private Displayy d;
+	private String name;
+	
+	public Testing(Displayy d, String name) {
+		this.d=d;
+		this.name=name;
+	}
+	
+	@Override
+	public void run() {
+		d.wishm1(name);
 	}
 }
 public class RevisionTest {
 	
+	public static int x() {
+		try {
+			System.out.println("HELLO");
+			//throw new Exception();
+			return 0;
+		}catch (Exception e) {
+			System.out.println("EXCEPTION");
+		}finally {
+			System.out.println("FINALLY");
+		}
+		return 1;
+	}
+	
 	public static void main(String[] args) {
-		Testing t=new Testing();
-		//Thread tobj=new Thread(t);
-			
+		System.out.println(x());
 	}	
 }
